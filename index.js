16ef9a60f24b24b23ed1aff3bb02e40e167b07cf
jeheo <jxford123@gmail.com>
2022-10-19 15:40:48 +0900

    mysql test

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql      = require('mysql');
+
 
 app.get('/', (req, res) => {
-  res.send('Hello it is me!')
-})
-
-
-const mysql      = require('mysql');
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Pooh(*)!123',
-  database : 'jeheo_server'
+  
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '0000',
+    database : 'jeheo_server'
+  });
+  connection.connect();
+  
+  connection.query('SELECT * from userinfo', (error, rows, fields) => {
+    if (error) throw error;
+    res.status(200).send(rows);
+    console.log('User info is: ', rows);
+    connection.end();
+  });
+  
 });
-
-connection.connect();
-
-connection.query('SELECT * from userinfo', (error, rows, fields) => {
-  if (error) throw error;
-  console.log('User info is: ', rows);
+app.get('/insert', (req, res) => {
+  
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '0000',
+    database : 'jeheo_server'
+  });
+  connection.connect();
+  
+  connection.query('insert * from userinfo', (error, rows, fields) => {
+    if (error) throw error;
+    res.send(rows);
+    console.log('User info is: ', rows);
+    connection.end();
+  });
+  
 });
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
-connection.end();
